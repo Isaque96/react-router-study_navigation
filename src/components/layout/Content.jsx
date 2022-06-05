eb51026d06b48c0500d9b3a49a949785661e2a52
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-06-05 12:07:32 -0300

    navigation between pages

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../../views/examples/Home";
+import About from "../../views/examples/About";
 import "./Content.css";
 
 const Content = (props) => {
   return (
     <main className="Content">
-      <h1>Component Content</h1>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/*
+        <Route path="/param/:id" element={<Param />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
     </main>
   );
 };
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Menu from "../components/layout/Menu";
 import Content from "../components/layout/Content";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 const App = (props) => {
   return (
     <div className="App">
-      <Menu />
-      <Content />
+      <BrowserRouter>
+        <Menu />
+        <Content />
+      </BrowserRouter>
     </div>
   );
 };
